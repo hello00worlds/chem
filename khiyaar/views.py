a5d3a70ee628b500f7cfa32efc0e8a1560acfc7b
emad <kazemi_emad@yahoo.com>
2019-09-06 16:54:35 +0430

    all pyc files removed

--- khiyaar/views.py
+++ khiyaar/views.py
@@ -14,14 +14,23 @@ def reactionToHtml(reac, prod):
     output = output[:-3]
     return output
 
-def weight(r, p):
+# def weight(r, p):
+#     for fractions in map(mass_fractions, [r, p]):
+#         return {k: '{0:.3g} wt%'.format(v*100) for k, v in fractions.items()}
+
+def weight(element, r, p, g):
+    wet = 0
     for fractions in map(mass_fractions, [r, p]):
-        return {k: '{0:.3g} wt%'.format(v*100) for k, v in fractions.items()}
+        for k, v in fractions.items():
+            if k == element:
+                wet = g/v*100
+    return {k: '{0:.3g} wt%'.format(v*100*g) for k, v in fractions.items()}
+
 
 def home(request):
     form = FormulaForm()
     formula = request.POST['formula'] if request.method == 'POST' else 'H2 + O2 -> H2O'
     r = Reaction.from_string(formula)
     reac, prod = balance_stoichiometry({sub for sub in r.reac}, {sub for sub in r.prod})
-    return render(request, 'formula.html', {'form': form, 'balanced': reactionToHtml(reac, prod), 'weight': weight(reac, prod)})
+    return render(request, 'formula.html', {'form': form, 'balanced': reactionToHtml(reac, prod), 'weight': weight('H2', reac, prod, 12)})
 
